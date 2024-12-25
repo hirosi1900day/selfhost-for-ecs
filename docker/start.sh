f021b534cbe26e7ab77980146c781cca46476024
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-12-25 10:50:59 +0900

    ghコマンドを使う形に変更

--- docker/start.sh
+++ docker/start.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-ACCESS_TOKEN=$ACCESS_TOKEN
-ORGANIZATION=$ORGANIZATION
-OWNER=$OWNER
-REPO=$REPO
-
 if [ -n "$ORGANIZATION" ]; then
   echo "get Organization token"
-  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+  REG_TOKEN=$(gh api -X POST /orgs/${ORGANIZATION}/actions/runners/registration-token --jq .token)
   cd /home/docker/actions-runner
   echo "setup Organization Runner"
-  ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}  
+  ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 else
   echo "get Repository token"
-  REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+  REG_TOKEN=$(gh api -X POST /repos/${OWNER}/${REPO}/actions/runners/registration-token --jq .token)
   cd /home/docker/actions-runner
   echo "setup Repository Runner"
-  ./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}  
+  ./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}
 fi
 
 cleanup() {
